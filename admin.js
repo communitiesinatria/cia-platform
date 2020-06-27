adc536cfcf1d2f4f5fa2f97d5c36b2f8f2d94f77
Raj <raj.fps2000@gmail.com>
2020-06-27 19:45:58 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -19,7 +19,8 @@ const x = {
     },
 }
 //resources or models
-const { UserModel, TeamMemberModel, ProjectModel, EventModel } = require('./controller/model')
+const { UserModel, TeamMemberModel, ProjectModel, EventModel } = require('./controller/model');
+const { request } = require('http');
 
 const adminBro = new AdminBro({
     resources: [
@@ -36,7 +37,7 @@ const adminBro = new AdminBro({
                             list: false, edit: !!1, filter: !1, show: !1,
                         },
                     },
-                    props:{
+                    props: {
                         isVisible: false
                     }
                 },
@@ -49,12 +50,8 @@ const adminBro = new AdminBro({
                                     password: x.encrypt(request.payload.setpassword),
                                     setpassword: undefined,
                                 }
-                            } else if (request.payload.links){
-                                request.payload={
-                                    ...request.payload,
-                                    //links:
-                                }
                             }
+
                             return request
                         },
                     }
@@ -63,19 +60,34 @@ const adminBro = new AdminBro({
 
         },
         TeamMemberModel,
-        ProjectModel,
-        EventModel
+        {
+            resource: ProjectModel,
+            options: {
+                actions: {
+                    new: {
+                        before: request => {
+                            if (request.payload.links) {
+                                request.payload = {
+                                    ...request.payload,
+                                    links: request.payload.links.split(',')
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            EventModel
     ],
     branding: {
         companyName: 'Communities in Atria',
-        logo:'https://instagram.fblr1-4.fna.fbcdn.net/v/t51.2885-19/s150x150/90442011_199456528028751_8249355337673474048_n.jpg?_nc_ht=instagram.fblr1-4.fna.fbcdn.net&_nc_ohc=AUnGVIFSfdcAX_VdXDl&oh=0f7e441346b9d3d56d027b435e238f20&oe=5F200B40',
-        theme:{
-            colors:{
-                primary100:'#00E9AA',
-                accent:'#00E9AA',
-                separator:'#454655',
+        logo: 'https://instagram.fblr1-4.fna.fbcdn.net/v/t51.2885-19/s150x150/90442011_199456528028751_8249355337673474048_n.jpg?_nc_ht=instagram.fblr1-4.fna.fbcdn.net&_nc_ohc=AUnGVIFSfdcAX_VdXDl&oh=0f7e441346b9d3d56d027b435e238f20&oe=5F200B40',
+        theme: {
+            colors: {
+                primary100: '#00E9AA',
+                accent: '#00E9AA',
+                separator: '#454655',
             }
-            
+
         }
     },
     rootPath: '/admin',
@@ -84,7 +96,7 @@ const adminBro = new AdminBro({
 const router = AdminBroExpressjs.buildAuthenticatedRouter(adminBro, {
     authenticate: async (email, password) => {
         const user = await UserModel.findOne({ email });
-        if (user.role==='admin') {
+        if (user.role === 'admin') {
 
             if (x.decrypt(user.password) === password) {
                 return user
