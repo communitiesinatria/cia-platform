40bf85687af9790e7dd246cf7b8ea016c4e0277b
Raj <raj.fps2000@gmail.com>
2020-06-27 13:28:56 +0530

    cia-deploy

--- controller/model/index.js
+++ controller/model/index.js
@@ -18,9 +18,11 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 //Get the default connection
 const db = mongoose.connection;
+
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('connected', () => console.log('database connected'));
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
