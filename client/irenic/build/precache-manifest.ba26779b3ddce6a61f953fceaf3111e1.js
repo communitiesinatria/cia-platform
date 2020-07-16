302242b7878c5e4c7799c4a828374570c12c6e10
Raj Sharma <xrehpicx@gmail.com>
2020-07-17 00:41:49 +0530

    Added settings page

---
+++ client/irenic/build/precache-manifest.ba26779b3ddce6a61f953fceaf3111e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e68da11d9650df1606f15939f06b4e",
+    "url": "/irenic/index.html"
+  },
+  {
+    "revision": "9741793bf47a29340c6e",
+    "url": "/irenic/static/css/main.c9a160ca.chunk.css"
+  },
+  {
+    "revision": "8dc0179fe3c11008a42d",
+    "url": "/irenic/static/js/2.355a11a0.chunk.js"
+  },
+  {
+    "revision": "f43306657146a4725905c486692acf40",
+    "url": "/irenic/static/js/2.355a11a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9741793bf47a29340c6e",
+    "url": "/irenic/static/js/main.9b5d9ec1.chunk.js"
+  },
+  {
+    "revision": "bef9fff05ff1da3f9af1",
+    "url": "/irenic/static/js/runtime-main.646b3e01.js"
+  },
+  {
+    "revision": "dc4ab841abcc650b19467cdfc3c715d0",
+    "url": "/irenic/static/media/irenic.dc4ab841.svg"
+  },
+  {
+    "revision": "f5c45a130c1cfcbf0ab1e5ba8b5dcd61",
+    "url": "/irenic/static/media/login-irenic.f5c45a13.svg"
+  },
+  {
+    "revision": "47ab12a00f1dc842d0ae989d006412f1",
+    "url": "/irenic/static/media/loginpage.47ab12a0.svg"
+  }
+]);
