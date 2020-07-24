6736e1686394da8973c5002a177ffaa52f983be6
Raj Sharma <xrehpicx@gmail.com>
2020-07-25 00:21:37 +0530

    better error catching

--- tests/db.js
+++ tests/db.js
@@ -1,4 +1,13 @@
-require('dotenv').config();
+try {
+    require('dotenv').config();
+} catch (error) {
+    console.log('.env module not needed');
+}
+
+if (!process.env.CIA_DATA_DB) {
+    console.log('MONGO URL NOT SPECIFIED IN .env file')
+    process.exit();
+}
 const mongoose = require('mongoose');
 module.exports = () => {
     const ciamedia = process.env.CIA_MEDIA_DB;
@@ -7,12 +16,12 @@ module.exports = () => {
         const cia_media_connection = mongoose.connection;
 
         //Bind connection to error event (to get notification of connection errors)
-        db.on('error', (e) => {  
-            console.log('DB connection failed',e);
+        db.on('error', (e) => {
+            console.log('DB connection failed', e);
             rej(1);
         });
         db.on('connected', () => {
-            
+
             console.log('DB connection successfull');
             res(0);
         });
