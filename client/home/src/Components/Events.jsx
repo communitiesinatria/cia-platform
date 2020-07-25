7d2f0be4346971bbe56db1207b2ea19cad31b8a6
suryamurugan <surya3542live@gmail.com>
2020-07-25 23:38:41 +0530

    Added events

--- client/home/src/Components/Events.jsx
+++ client/home/src/Components/Events.jsx
@@ -1,17 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, useHistory, Route } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, useHistory, Route } from 'react-router-dom';
 
-import "./css/Events.css";
+import './css/Events.css';
+import { getEvents } from './api';
 
 export default function Events() {
+  const [events, setevents] = useState([]);
+
+  useEffect(() => {
+    getEvents().then((d) => {
+      console.log(d);
+      setevents(d);
+    });
+  }, []);
+
   return (
     <Router>
       <Route path="/events" exact={true}>
         <div className="events">
           <div className="this-week">
-            <EventMain />
+            <EventMain {...events[0]} />
           </div>
-          <AllEvents />
+          <AllEvents events={events} />
         </div>
       </Route>
       <Route path="/events/view">
@@ -21,26 +31,19 @@ export default function Events() {
   );
 }
 
-function EventMain() {
+function EventMain({ title, desc, register, event_img, date }) {
   return (
     <div className="main-event">
       <div className="event-img">
-        <img
-          src="https://engineering.tamu.edu/student-life/_files/_images/_feature-images/speaker-talk-11Oct2018.jpg"
-          alt=""
-        />
+        <img src={event_img} alt="" />
       </div>
-      <div className="about">
-        <h1>Event title</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore neque
-          blanditiis veritatis repellendus! Veniam, veritatis fugiat Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Tempore neque blanditiis
-          veritatis repellendus! Veniam, veritatis fugiat Lorem ipsum dolor sit
-        </p>
-        <span className="date-time">3rd Jan 2020</span>
 
-        <a target="_blank" href="/" className="register-btn">
+      <div className="about">
+        <h1>{title}</h1>
+        <p>{desc}</p>
+        <span className="date-time">{date}</span>
+
+        <a target="_blank" href={register} className="register-btn">
           <span>Register</span>
         </a>
       </div>
@@ -48,69 +51,87 @@ function EventMain() {
   );
 }
 
-function AllEvents() {
-  const test = Array.from({ length: 10 });
+function AllEvents({ events }) {
+  // const test = Array.from({ length: 10 });
   return (
     <div className="all-events">
-      {test.map((e, i) => (
-        <Event key={i} />
+      {events.map((e, i) => (
+        <Event event={e} key={i} />
       ))}
     </div>
   );
 }
 
-function Event() {
+function Event({ event }) {
+  const date = timeConverter(event.date);
+  event.date = date;
+  console.log(date);
+
   const history = useHistory();
 
-  const event = { title: "test title" };
+  //const event = { title: "test title" };
   return (
     <div
       className="event"
       onClick={() => {
-        history.push({ pathname: "events/view", data: event });
+        history.push({ pathname: 'events/view', data: event });
       }}
     >
       <div className="bg-img">
-        <img
-          src="https://total-event.com/wp-content/uploads/2018/01/event-planning-microsoft-ignite.jpg"
-          alt="eventimage"
-        />
+        <img src={event.event_img} alt="eventimage" />
       </div>
       <div className="about">
-        <h1>title</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia laborum
-        </p>
-        <span className="date-time">3rd Jan 2020</span>
+        <h1>{event.title}</h1>
+        <p>{event.desc}</p>
+        <span className="date-time">{date}</span>
       </div>
     </div>
   );
+}
+function timeConverter(date) {
+  var a = new Date(date);
+  var months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
+  return time;
 }
 
 function EventView() {
   const history = useHistory();
 
   const event = history.location.data;
+
   return (
     <div className="event-view">
       <div className="event-img">
-        <img
-          src="https://engineering.tamu.edu/student-life/_files/_images/_feature-images/speaker-talk-11Oct2018.jpg"
-          alt=""
-        />
+        <img src={event.event_img} alt="" />
       </div>
       <div className="about">
-        <h1>Event title</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore neque
-          blanditiis veritatis repellendus! Veniam, veritatis fugiat Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Tempore neque blanditiis
-        </p>
-        <span className="date-time">3rd Jan 2020</span>
+        <h1>{event.title}</h1>
+        <p>{event.desc}</p>
+        <span className="date-time">{event.date}</span>
 
         <div className="register-btn">
           <span>
-            <a href="">Register</a>
+            <a href={event.register}>Register</a>
           </span>
         </div>
       </div>
